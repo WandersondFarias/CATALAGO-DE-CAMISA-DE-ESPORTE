058a28a9a947132cf47580fc80fd58fae6655252
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-21 20:23:54 -0300

    Create spain.js

---
+++ items/spain.js
@@ -0,0 +1,36 @@
+const productsEspanha = [
+    {   
+        Id: 0,
+        Nome: 'Camisa Barcelona',
+        Ano: '2020/2021',
+        Imagem: './imagens/barcelona.jpg',
+        Preço: 129.9,
+        Quantidade: 0
+    },
+    {
+        Id: 1,
+        Nome: 'Camisa Celta de Vigo',
+        Ano: '2020/2021',
+        Imagem: './imagens/celtadevigo.jpg',
+        Preço: 129.9,
+        Quantidade: 0
+    },
+    {   
+        Id: 2,
+        Nome: 'Camisa Real Madrid',
+        Ano: '2020/2021',
+        Imagem: './imagens/realmadrid.jpg',
+        Preço: 149.9,
+        Quantidade: 0
+    },
+    {   
+        Id: 3,
+        Nome: 'Camisa Atlético de Madrid',
+        Ano: '2020/2021',
+        Imagem: './imagens/atlmadrid.jpg',
+        Preço: 229.9,
+        Quantidade: 0
+    }
+]
+
+module.exports = productsEspanha
