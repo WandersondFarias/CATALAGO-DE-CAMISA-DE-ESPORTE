8996e29b235b5f9012eabd4cb16ac068bd11f074
wandersondefarias@programador Front- end-HTML-CSS-JAVASCRIPT <96745976+wandersondefariasprogramador@users.noreply.github.com>
2022-03-21 20:23:40 -0300

    Create brazil.js

---
+++ items/brazil.js
@@ -0,0 +1,32 @@
+const productsBrazil =   [
+    {   
+    Id: 8,
+    Nome: 'Camisa Vasco White 2020/2021',
+    Imagem: './imagens/basco-white.jfif',
+    Preço: 'R$ 109,90',
+    Quantidade: 0,
+    Categoria: 'Brasil'
+},
+]
+
+/* 
+<div class="box-product">
+    <img src="./imagens/flamengo.jfif" alt="" class="img-product">
+    <p class="text-box">Camisa Flamengo 2020/2021</p>
+    <p class="price" id="price">R$ 129,90<p>
+    </div>
+    <div class="box-product">
+    <img src="./imagens/sport-white.jfif" alt="" class="img-product">
+    <p class="text-box">Camisa Sport White 2020/2021</p>
+    <p class="price" id="price">R$ 149,90<p>
+    </div>
+    <div class="box-product">
+    <img src="./imagens/palmeiras-white.jfif" alt="" class="img-product">
+    <p class="text-box">Camisa Palmeiras White 2019/2020</p>
+    <p class="price" id="price">R$ 199,90<p>
+    </div>
+    <div class="box-product">
+    <img src="./imagens/vasco-white.jfif" alt="" class="img-product">
+    <p class="text-box">Camisa Vasco White 2020/2021</p>
+    <p class="price" id="price">R$ 99,90<p>
+    </div> ` */
